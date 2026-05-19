43c1095e935a353073061d7e7571af35e899c18d
Victor <ander37alex@gmail.com>
2026-05-18 19:27:06 -0500

    Restrict ticket closing to staff

--- commands/closeticket.js
+++ commands/closeticket.js
@@ -1,7 +1,11 @@
 const { EmbedBuilder } = require('discord.js');
+const { hasModRoleOrHigher } = require('../utils/modPermissions');
 module.exports = {
     name: 'closeticket',
     async execute(message) {
+        if (!hasModRoleOrHigher(message.member)) {
+            return message.reply({ embeds: [new EmbedBuilder().setColor('Red').setDescription('❌ Solo moderadores o superiores pueden cerrar tickets.')] });
+        }
         if (!message.channel.name.includes('ticket')) {
             return message.reply({ embeds: [new EmbedBuilder().setColor('Red').setDescription('❌ Este comando solo se puede usar dentro de un canal de ticket.')] });
         }
